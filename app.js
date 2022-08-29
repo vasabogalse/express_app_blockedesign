e406499eda743aaf00502378e18e777584246d3b
vasabogalse <vsabogalserrano@gmail.com>
2022-08-28 19:31:19 -0500

    bootstrap install

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public/stylesheets', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
