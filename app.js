258a57e7f01c7b03efc34c462d439011492613be
vasabogalse <vsabogalserrano@gmail.com>
2022-09-12 19:39:13 -0500

    vars to const

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var createProjectRouter = require('./routes/createProject');
-//const { hasSubscribers } = require('diagnostics_channel');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const createProjectRouter = require('./routes/createProject');
+
 const directoryPartials = path.join(__dirname, 'partials');
 
 const app = express();
@@ -30,12 +30,12 @@ app.use('/users', usersRouter);
 app.use('/createProject', createProjectRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
